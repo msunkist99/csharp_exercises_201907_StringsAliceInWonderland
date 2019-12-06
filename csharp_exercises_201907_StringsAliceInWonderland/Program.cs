24f3cf4aa027b83e04943e612af987b199335a79
msunkist99 <msunkist99@gmail.com>
2019-12-06 15:30:01 -0600

    Add project files.

---
+++ csharp_exercises_201907_StringsAliceInWonderland/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace csharp_exercises_201907_StringsAliceInWonderland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aliceInWonderland = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+
+            Console.WriteLine("Enter your search string - ");
+            string searchString = Console.ReadLine();
+
+            int index = aliceInWonderland.ToUpper().IndexOf(searchString.ToUpper());
+
+            //int index = aliceInWonderland.ToUpper().IndexOf(Console.ReadLine().ToUpper());
+
+            if (index > 0)
+            {
+                Console.WriteLine("Your search string was found at index - " + index);
+            }
+            else
+            {
+                Console.WriteLine("Your search string was NOT found!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
